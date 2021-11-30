7e8e015d14b17c44dc76df2ca28a384faa39ce52
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-30 20:06:32 +0600

    2nd commit

---
+++ index.py
@@ -0,0 +1,66 @@
+# v1 = int(input('Enter 1st car is velocity: '))
+# v2 = int(input('Enter 2nd car is velocity: '))
+# time = int(input('Enter time: '))
+
+# distance = (v1 + v2) * time
+# print(distance)
+
+#define = def 
+
+# v1 = int(input('Enter 1st car is velocity: '))
+# v2 = int(input('Enter 2nd car is velocity: '))
+# time = int(input('Enter time: '))
+# def calculateDistance (v1, v2, t):
+#     return (v1 + v2) * t
+
+# # d = calculateDistance(70 , 100, 2)
+# # print(d)
+# print(calculateDistance(70 , 100, 2))
+
+
+
+# def calculateDistance (v1, v2, t):
+#     return (v1 + v2) * t
+
+# v1 = int(input('Enter 1st car is velocity: '))
+# v2 = int(input('Enter 2nd car is velocity: '))
+# time = int(input('Enter time: '))
+
+# print(calculateDistance(v1, v2, time))
+
+
+# def calculateSquare(n):
+#     return (n ** 2)
+# print(calculateSquare(7))
+
+# def calculateSquare(n):
+#     return (n * n)
+# print(calculateSquare(7))
+
+
+# def calcPower(base, exp):
+#     return base ** exp
+# print(calcPower(3, 4))
+
+
+# def reverseList(list):
+#     return(list [::-1])
+# print (reverseList([3, 2, 1]))
+
+
+#reciprocal
+
+# def calcDevide(n):
+#     return (n ** (-1))
+
+# print(calcDevide(5))
+
+
+
+
+
+def calc(n, n1):
+    return (n + n1)
+
+print(calc(5, 6))
+        
